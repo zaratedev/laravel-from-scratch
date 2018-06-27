a3149258c5785ff2f32b46e53c09b0e9d159acef
Jonathan Zarate <zaratedev@gmail.com>
2018-06-27 13:48:24 -0500

    Add TasksController and Clean file routes/web.php

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    public function index() {
+
+        $tasks = Task::incomplete()->get();
+
+        return view('tasks.index', compact('tasks'));
+    }
+
+    public function show($id) {
+        $task = Task::find($id);
+
+        return view('tasks.show', compact('task'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,19 +11,5 @@
 |
 */
 
-Route::get('/tasks', function () {
-
-    $tasks = \App\Task::all();
-
-    $tasks = \App\Task::incomplete()->get();
-
-    return view('tasks.index', compact('tasks'));
-});
-
-
-Route::get('tasks/{id}', function ($id) {
-
-    $task = \App\Task::find($id);
-
-    return view('tasks.show', compact('task'));
-});
+Route::get('/tasks', 'TasksController@index');
+Route::get('tasks/{id}', 'TasksController@show');
