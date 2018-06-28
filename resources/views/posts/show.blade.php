ab9ce01f73026154227055378d06fc7ebd7c668f
Jonathan Zarate <zaratedev@gmail.com>
2018-06-27 19:38:23 -0500

    Eloquent Relationships and Comments.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,6 +7,21 @@
         <p class="text-muted">
             {{ $post->body }}
         </p>
+        <hr>
+        <strong>Comments!</strong>
+        <hr>
+        <div class="comments">
+            <ul class="list-group">
+                @foreach($post->comments as $comment)
+                    <li class="list-group-item">
+                        <strong>
+                            {{ $comment->created_at->diffForHumans() }}
+                        </strong>
+                        {{ $comment->body }}
+                    </li>
+                @endforeach
+            </ul>
+        </div>
     </div>
 
 @endsection
