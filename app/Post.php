6f5f980970bb00e63eeda302d6c4195bf791b4c0
Jonathan Zarate <zaratedev@gmail.com>
2018-06-28 11:36:41 -0500

    Add colunm user_id to table post and comments.

--- app/Post.php
+++ app/Post.php
@@ -12,6 +12,10 @@ class Post extends Model
         return $this->hasMany(Comment::class);
     }
 
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
     public function addComment($body) {
 
         $this->comments()->create(compact('body'));
